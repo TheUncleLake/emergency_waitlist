cdb34e9aab3e6b9cb2323cf9123e4484972465f6
TheUncleLake <100882470+TheUncleLake@users.noreply.github.com>
2024-07-30 14:12:13 -0400

    added basic api call

---
+++ public/assets/script.js
@@ -0,0 +1,19 @@
+(function() {
+    const getpatients = document.getElementById("getpatients");
+    const patients = document.getElementById("patients");
+    getpatients.onclick = async function() {
+        $.ajax({type: "GET", url: "api/getpatients"}).then(function(data) {
+            if (typeof data === "string") {
+                alert(data);
+                return;
+            }
+            patients.innerHTML = "";
+            for (const elem of data) {
+                let tr = patients.insertRow();
+                tr.insertCell().innerHTML = elem.patient_name;
+                tr.insertCell().innerHTML = elem.severity;
+                tr.insertCell().innerHTML = elem.date_triage;
+            }
+        });
+    };
+})();
